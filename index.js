11c5c58f9a83f838f90bcabf48eda1d2d03fdb0c
Eidmantas Ivanauskas <eidmantas.ivanauskas@gmail.com>
2017-04-17 19:27:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var downloader = require('spotify-downloader');
 var readlineSync = require('readline-sync');
 
-var user = readlineSync.question("Enter user");
-var playlistId = readlineSync.question("Enter playlistId");
+var user = readlineSync.question("Enter user ");
+var playlistId = readlineSync.question("Enter playlistId ");
+var magicalHackeryTimeout = readlineSync.question("Delay between downloads. Try 0 or 5000 ");
 
 downloader('spotify:user:' + user  + ':playlist:' + playlistId, function() {}, function (err){
-  });
+  }, magicalHackeryTimeout);
